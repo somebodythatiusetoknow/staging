90171f14059025005930976695bbd58807684de2
somebodythatiusetoknow <122755740+somebodythatiusetoknow@users.noreply.github.com>
2025-07-08 10:38:32 +0330

    Update [[catchall]].js

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,5 +1,19 @@
 export async function onRequest(context) {
-  return new Response(JSON.stringify(context.request.cf, null, 2), {
-    headers: { 'content-type': 'application/json' },
-  });
+  try {
+    const { request } = context;
+
+    const cfInfo = JSON.stringify(request.cf, null, 2);
+    console.log('CF Info:', cfInfo); 
+
+    const url = new URL(request.url);
+    const splitted = url.pathname.replace(/^\/+/, '').split('/');
+    const address = splitted[0];
+    url.pathname = '/' + splitted.slice(1).join('/');
+    url.hostname = address;
+    url.protocol = 'https';
+
+    return fetch(new Request(url, request));
+  } catch (e) {
+    return new Response('Error: ' + e.message, { status: 500 });
+  }
 }
