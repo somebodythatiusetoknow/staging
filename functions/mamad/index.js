16322ed56c61c7f09e30952b6f17311a74413c26
somebodythatiusetoknow <122755740+somebodythatiusetoknow@users.noreply.github.com>
2025-07-08 22:15:42 +0330

    Update index.js

--- functions/mamad/index.js
+++ functions/mamad/index.js
@@ -1,17 +1,3 @@
-export async function onRequest({ request }) {
-  try {
-    const url = new URL(request.url);
-    const parts = url.pathname.replace(/^\/+/, '').split('/');
-
-    const targetHost = parts[1];
-    const newPath = '/' + parts.slice(2).join('/');
-
-    url.hostname = targetHost;
-    url.pathname = newPath;
-    url.protocol = 'https';
-
-    return fetch(new Request(url.toString(), request));
-  } catch (e) {
-    return new Response('Proxy error: ' + e.message, { status: 500 });
-  }
+export async function onRequest() {
+  return new Response('Hello from mamad function!');
 }
