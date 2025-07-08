9c9eefb36a15d72f2ed0adb87224c75bad6d9ff6
somebodythatiusetoknow <122755740+somebodythatiusetoknow@users.noreply.github.com>
2025-07-08 21:58:26 +0330

    Update [[catchall]].js

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,13 +1,1 @@
-export async function onRequest({ request }) {
-  try {
-    const url = new URL(request.url);
-    const splitted = url.pathname.replace(/^\/+/, '').split('/');
-    const address = splitted[0];
-    url.pathname = '/' + splitted.slice(1).join('/');
-    url.hostname = address;
-    url.protocol = 'https';
-    return fetch(new Request(url, request));
-  } catch (e) {
-    return new Response('Error: ' + e.message, { status: 500 });
-  }
-}
+
